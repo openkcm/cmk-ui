2c6fd718117d9b5e26c1b35c2ae34623cac8cb45
Rohan Vembar <rohan.vembar@sap.com>
2024-11-13 14:40:50 -0800

    Add key config creation
    KMS20-950

--- mock.mjs
+++ mock.mjs
@@ -5,6 +5,7 @@ import systems from "./mockServer/mockdata/systems.mjs";
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 
 app.get("/api/v1/keyConfig", (req, res) => {
     res.json(keyConfig());
@@ -18,6 +19,11 @@ app.patch("/api/v1/keyConfig", (req, res) => {
 app.post("/api/v1/keys", (req, res) => {
     const newKey = req.body;
     res.status(201).json(newKey);
+});
+
+app.post("/api/v1/keyConfig", (req, res) => {
+    req.body.id = "b40218e3-be88-5eed-9f59-b2066938e2fa";
+    res.status(201).json(req.body);
 });
 
 app.get("/api/v1/systems", (req, res) => {
