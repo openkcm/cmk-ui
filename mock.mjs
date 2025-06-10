d5e98eecfcab71ff6bd713cf221b349f8c193a74
Neil McClelland <neil.mcclelland@sap.com>
2025-06-10 14:58:53 +0100

    KMS20-2230:Remove additional trailing slash on env URLs

--- mock.mjs
+++ mock.mjs
@@ -14,97 +14,97 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/api/v1/keyConfigurations/:id?", (req, res) => {
+app.get("/api/v1/tenant1/keyConfigurations/:id?", (req, res) => {
     const { id } = req.params;
     res.json(keyConfig(id));
 });
 
-app.get("/api/v1/keyConfigurations/:id/tags", (req, res) => {
+app.get("/api/v1/tenant1/keyConfigurations/:id/tags", (req, res) => {
     const { id } = req.params;
     res.json(tags(id));
 });
 
-app.put("/api/v1/keyConfigurations/:id/tags", (req, res) => {
+app.put("/api/v1/tenant1/keyConfigurations/:id/tags", (req, res) => {
     const { id } = req.params;
     res.status(204).json(tags(id));
 });
 
-app.put("/api/v1/keyConfig/:id/primaryKey", (req, res) => {
+app.put("/api/v1/tenant1/keyConfig/:id/primaryKey", (req, res) => {
     res.status(204).json(keys());
 });
 
-app.patch("/api/v1/keyConfigurations/:id", (req, res) => {
+app.patch("/api/v1/tenant1/keyConfigurations/:id", (req, res) => {
     const newConfig = req.body;
     res.status(200).json(newConfig);
 });
 
-app.post("/api/v1/keys", (req, res) => {
+app.post("/api/v1/tenant1/keys", (req, res) => {
     const newKey = req.body;
     res.status(201).json(newKey);
 });
 
-app.get("/api/v1/keys/:id?", (req, res) => {
+app.get("/api/v1/tenant1/keys/:id?", (req, res) => {
     const { id } = req.params;
     res.json(keys(id));
 });
 
-app.delete("/api/v1/keys/:id", (req, res) => {
+app.delete("/api/v1/tenant1/keys/:id", (req, res) => {
     res.status(204).json({});
 });
 
-app.patch("/api/v1/keys/:id", (req, res) => {
+app.patch("/api/v1/tenant1/keys/:id", (req, res) => {
     const { id } = req.params;
     res.status(200).json(keys(id));
 });
 
-app.get("/api/v1/keys/:id/versions", (req, res) => {
+app.get("/api/v1/tenant1/keys/:id/versions", (req, res) => {
     const { keyID } = req.params;
     res.json(keyVersions(keyID));
 });
 
-app.post("/api/v1/keys/:id/versions", (req, res) => {
+app.post("/api/v1/tenant1/keys/:id/versions", (req, res) => {
     res.status(200).json({});
 });
 
-app.patch("/api/v1/keys/:id/versions/:id", (req, res) => {
+app.patch("/api/v1/tenant1/keys/:id/versions/:id", (req, res) => {
     res.status(200).json({});
 });
 
-app.get("/api/v1/systems/:systemId?/:keyConfigurationID?", (req, res) => {
+app.get("/api/v1/tenant1/systems/:systemId?/:keyConfigurationID?", (req, res) => {
     const { systemId, keyConfigurationID } = req.query;
     res.json(systems(systemId, keyConfigurationID));
 });
 
-app.patch("/api/v1/systems/:systemId?/link/", (req, res) => {
+app.patch("/api/v1/tenant1/systems/:systemId?/link/", (req, res) => {
     const { systemId, keyConfigurationID } = req.query;
     res.json(systems(systemId, keyConfigurationID));
 });
 
-app.delete("/api/v1/systems/:systemId?/link/", (req, res) => {
+app.delete("/api/v1/tenant1/systems/:systemId?/link/", (req, res) => {
     const { systemId, keyConfigurationID } = req.query;
     res.json(systems(systemId, keyConfigurationID));
 });
 
-app.get("/api/v1/groups", (req, res) => {
+app.get("/api/v1/tenant1/groups", (req, res) => {
     res.json(groups());
 });
 
-app.get("/api/v1/user/:id?", (req, res) => {
+app.get("/api/v1/tenant1/user/:id?", (req, res) => {
     const { id } = req.params;
     res.json(user(id));
 });
 
-app.get("/api/v1/workflows/:taskId?", (req, res) => {
+app.get("/api/v1/tenant1/workflows/:taskId?", (req, res) => {
     const { taskId } = req.params;
     res.json(workflows(taskId));
 });
 
-app.get("/api/v1/workflows/:taskId/approvers", (req, res) => {
+app.get("/api/v1/tenant1/workflows/:taskId/approvers", (req, res) => {
     const { taskId } = req.params;
     res.json(approvers(taskId));
 });
 
-app.post("/api/v1/workflows/:taskId/state", (req, res) => {
+app.post("/api/v1/tenant1/workflows/:taskId/state", (req, res) => {
     const state = req.body;
     res.status(200).json(state);
 });
