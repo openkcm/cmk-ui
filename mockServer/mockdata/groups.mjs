2ed348c4b0693bd40754df41ad1f092e20791fe6
Rohan Vembar <rohan.vembar@sap.com>
2025-08-28 12:23:21 -0700

    KMS20-2548: Show IAM ID on groups page

--- mockServer/mockdata/groups.mjs
+++ mockServer/mockdata/groups.mjs
@@ -1,23 +1,13 @@
 import Chance from "chance";
 const chance = new Chance();
 export default () => {
-    const generateUsers = (count) => {
-        return Array.from({ length: count }, () => (
-            {
-                id: chance.guid(),
-                name: chance.name(),
-                email: chance.email(),
-                count: count
-            }
-        ));
-
-    };
     const generateGroups = (count) => {
         const response = Array.from({ length: count }, () => ({
             name: chance.pickone(['KMS Admin', 'KMS Admin EU', 'KMS Admin US', 'KMS Auditor']),
             description: chance.sentence(),
             id: chance.guid(),
             role: chance.pickone(['Tenant Administrator', 'Auditor']),
+            iamIdentifier: chance.string({ length: 10, pool: 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789' })
         }));
         return {
             value: response,
