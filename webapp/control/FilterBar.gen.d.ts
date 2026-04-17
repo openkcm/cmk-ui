bbb4f7a5bc5e4e9a0d3c5750d5691e5f83a8a44b
neilmcclelland <neil.mcclelland@sap.com>
2026-04-17 14:03:25 +0100

    Remove Go button on systems filter (#78)

--- webapp/control/FilterBar.gen.d.ts
+++ webapp/control/FilterBar.gen.d.ts
@@ -14,7 +14,6 @@ declare module "./FilterBar" {
         title?: string | PropertyBindingInfo;
         filters?: Control[] | Control | AggregationBindingInfo | `{${string}}`;
         layoutForm?: SimpleForm;
-        search?: (event: FilterBar$SearchEvent) => void;
         reset?: (event: FilterBar$ResetEvent) => void;
     }
 
@@ -38,12 +37,6 @@ declare module "./FilterBar" {
         setLayoutForm(layoutForm: SimpleForm): this;
         destroyLayoutForm(): this;
 
-        // event: search
-        attachSearch(fn: (event: FilterBar$SearchEvent) => void, listener?: object): this;
-        attachSearch<CustomDataType extends object>(data: CustomDataType, fn: (event: FilterBar$SearchEvent, data: CustomDataType) => void, listener?: object): this;
-        detachSearch(fn: (event: FilterBar$SearchEvent) => void, listener?: object): this;
-        fireSearch(parameters?: FilterBar$SearchEventParameters): this;
-
         // event: reset
         attachReset(fn: (event: FilterBar$ResetEvent) => void, listener?: object): this;
         attachReset<CustomDataType extends object>(data: CustomDataType, fn: (event: FilterBar$ResetEvent, data: CustomDataType) => void, listener?: object): this;
@@ -52,23 +45,11 @@ declare module "./FilterBar" {
     }
 
     /**
-     * Interface describing the parameters of FilterBar's 'search' event.
-     */
-    // eslint-disable-next-line
-    export interface FilterBar$SearchEventParameters {
-    }
-
-    /**
      * Interface describing the parameters of FilterBar's 'reset' event.
      */
     // eslint-disable-next-line
     export interface FilterBar$ResetEventParameters {
     }
-
-    /**
-     * Type describing the FilterBar's 'search' event.
-     */
-    export type FilterBar$SearchEvent = Event<FilterBar$SearchEventParameters>;
 
     /**
      * Type describing the FilterBar's 'reset' event.
